2061c5d520b5f8df91f6c3a5bfd41dcf590d052d
Clint Grove <clintelyonson@gmail.com>
2022-10-05 11:38:21 +0100

    rg name

--- Synapse_tf_modules/resource_group.tf
+++ Synapse_tf_modules/resource_group.tf
@@ -1,8 +1,3 @@
 module "resource_group" {
-  source = "github.com/murggu/azure-terraform-modules/resource-group"
-
-  location = var.location
-
-  prefix  = var.prefix
-  postfix = random_string.postfix.result
+  name = "rg-syn-ooctc4"
 }

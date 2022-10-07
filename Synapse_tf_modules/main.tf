06cd1d47482e0d8733135c767d65696ffca01e15
Clint Grove <clintelyonson@gmail.com>
2022-10-07 14:11:00 +0100

    tfstate baby

--- Synapse_tf_modules/main.tf
+++ Synapse_tf_modules/main.tf
@@ -4,7 +4,14 @@ terraform {
       version = "= 2.83.0"
     }
   }
+  backend "azurerm" {
+  resource_group_name = "NetworkWatcherRG" ##"lz-data-${lower(var.environment)}-uks-rg-01"
+  storage_account_name = "${lower(var.environment)}terrastate919"
+  container_name = "tfstate-syn2"
+  key = "terraform.tfstate"
 }
+}
+
 
 provider "azurerm" {
   features {}

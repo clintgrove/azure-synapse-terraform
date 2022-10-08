ad5611050f1ba3c92bf4448b456be6e8d3277f3f
Clint Grove <clintelyonson@gmail.com>
2022-10-08 07:41:17 +0100

    allow stg

--- storage-account/main.tf
+++ storage-account/main.tf
@@ -43,7 +43,7 @@ resource "azurerm_storage_account_network_rules" "firewall_rules" {
   resource_group_name  = var.rg_name
   storage_account_name = azurerm_storage_account.syn_st.name
 
-  default_action             = "Deny"
+  default_action             = "Allow"
   ip_rules                   = [data.http.ip.body]
   virtual_network_subnet_ids = var.firewall_virtual_network_subnet_ids
   bypass                     = var.firewall_bypass

b868b36f5c316dfaf8f95ee28d678798def59be2
Clint Grove <clintelyonson@gmail.com>
2022-10-08 07:20:22 +0100

    out

--- storage-account/output.tf
+++ storage-account/output.tf
@@ -6,6 +6,6 @@ output "name" {
   value = azurerm_storage_account.syn_st.name
 }
 
-#output "adls_id" {
-#  value = azurerm_storage_data_lake_gen2_filesystem.st_adls.id
-#}
+output "adls_id" {
+  value = azurerm_storage_data_lake_gen2_filesystem.st_adls.id
+}
